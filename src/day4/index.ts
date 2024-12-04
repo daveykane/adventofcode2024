b2fc621bb38d768cb2899c7193bfd6684378fe7c
David Kane <david@osk.ltd>
2024-12-04 22:59:38 +0000

    feat: tidy up

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -1,4 +1,4 @@
-const diagonals = { UR: [1, -1], DR: [1, 1], DL: [-1, 1], UL: [-1, -1] };
+const diagonals = { UL: [-1, -1], UR: [1, -1], DR: [1, 1], DL: [-1, 1] };
 const directions = { ...diagonals, U: [0, -1], R: [1, 0], D: [0, 1], L: [-1, 0] };
 
 const getChar = (wordsearch: string[][], y: number, x: number) => wordsearch[y] && wordsearch[y][x];
@@ -29,13 +29,13 @@ export const part2 = (wordsearch: string[][]) => {
   for (let i = 0; i < wordsearch.length; i++) {
     for (let j = 0; j < wordsearch[i].length; j++) {
       if (wordsearch[i][j] === "A") {
-        const [tr, br, bl, tl] = Object.values(diagonals).map(([dx, dy]) =>
-          getNextChars(wordsearch, i, j, [dx, dy], [1])
+        count += Number(
+          ["MMSS", "MSSM", "SSMM", "SMMS"].includes(
+            Object.values(diagonals)
+              .map((dd) => getNextChars(wordsearch, i, j, dd, [1]))
+              .join("")
+          )
         );
-
-        if ((tl + br === "MS" || tl + br === "SM") && (bl + tr === "MS" || bl + tr === "SM")) {
-          count += 1;
-        }
       }
     }
   }
